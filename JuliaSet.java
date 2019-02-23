52a9399366e430edc6743d75a8050a33e96a846f
Swapnil N <swapnil.napuri@gmail.com>
2019-02-23 12:42:28 -0500

    Finished basic set up

--- JuliaSet.java
+++ JuliaSet.java
@@ -6,32 +6,33 @@ import java.awt.image.BufferedImage;
 public class JuliaSet extends JPanel implements AdjustmentListener, ActionListener{
 
     JFrame frame;
-    JScrollBar[] scrollBar;
-    int[] color;
+    JScrollBar[] scrollBarArr;
     JCheckBox invert;
     Boolean flip = true;
+
     int height;
     int width;
+    double A = 0;
+    double B = 0;
 
-    float maxInter = 300;
+    float maxIter = 300;
     
     public JuliaSet(){
 
         frame = new JFrame("Julia Set Program");
         frame.add(this);
-        scrollBar = new JScrollBar[3];
-        color = new int[3];
+        scrollBarArr = new JScrollBar[5];
         invert = new JCheckBox();
         invert.addActionListener(this);
 
         JPanel scrollPanel = new JPanel();
         scrollPanel.setLayout(new GridLayout(1, 3));
 
-        for (int x=0;x<3;x++){
-            scrollBar[x] = new JScrollBar(JScrollBar.VERTICAL, 0, 0, 0, 255);
-            scrollBar[x].addAdjustmentListener(this);
-            scrollPanel.add(scrollBar[x]);
-            scrollBar[x].setUnitIncrement(1);
+        for (int x=0;x<5;x++){
+            scrollBarArr[x] = new JScrollBar(JScrollBar.VERTICAL, 0, 0, 0, 255);
+            scrollBarArr[x].addAdjustmentListener(this);
+            scrollPanel.add(scrollBarArr[x]);
+            scrollBarArr[x].setUnitIncrement(1);
         }
 
         JPanel onePanel = new JPanel();
@@ -52,8 +53,7 @@ public class JuliaSet extends JPanel implements AdjustmentListener, ActionListen
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
-        g.setColor(new Color(color[0],color[1],color[2]));
-        g.fillRect(0, 0, frame.getWidth(), frame.getHeight());
+        //g.fillRect(0, 0, frame.getWidth(), frame.getHeight());
 
         drawJuliaSet(g);
 
@@ -61,28 +61,42 @@ public class JuliaSet extends JPanel implements AdjustmentListener, ActionListen
 
     public void drawJuliaSet(Graphics g) {
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        width = frame.getWidth();
+        height = frame.getHeight();
 
-        for (int x=0;x<width;x++){
+        for (int x=0;x<width;x++){           
             for (int y=0;y<height;y++){
-                float zoom = 1;
-                float itFloat = maxInter;
 
-                double zx = 1.5*((x-0.5*x)/(0.5*zoom*width));
-                double zy = (y-.5*y)/(.5*zoom*height);
+                float zoom = 1f;
+                float counter = maxIter;
 
-                //while ((Math.pow(zx, 2)+Math.pow(zy, 2))<6 && itFloat>0){
+                double zx = 1.5*((x-0.5*width)/(0.5*zoom*width));
+                double zy = (y-0.5*height)/(.5*zoom*height);
 
-                //}
+                while ((Math.pow(zx,2) + Math.pow(zy,2))<6 && counter>0){
+                    double temp = Math.pow(zx, 2) - Math.pow(zy, 2) + A;
+                    zy = (2*zx*zy) + B;
+                    zx = temp;
+                    counter--;
+                }
 
+                int c;
+                if(counter>0)
+                    c = Color.HSBtoRGB((maxIter/counter)%1, 1, 1);
+                else 
+                    c = Color.HSBtoRGB(maxIter/counter,1, 0);
+
+                image.setRGB(x, y, c);
             }
         }
+        g.drawImage(image, 0, 0, null);
     }
 
     public void adjustmentValueChanged(AdjustmentEvent e) {
 
         for (int i=0;i<3;i++){
-            if(e.getSource() == scrollBar[i]){
-                color[i] = scrollBar[i].getValue();
+            if(e.getSource() == scrollBarArr[i]){
+                //color[i] = scrollBar[i].getValue();
             }
         }
 
@@ -92,7 +106,6 @@ public class JuliaSet extends JPanel implements AdjustmentListener, ActionListen
     public void actionPerformed(ActionEvent e){
         if (e.getSource() == invert){
             for (int i=0;i<3;i++){
-                color[i] = 255 - color[i];
             }
 
             /*if (flip){
